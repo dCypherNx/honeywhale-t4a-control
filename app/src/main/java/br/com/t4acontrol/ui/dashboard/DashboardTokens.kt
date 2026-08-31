6ad526144d1a83bc512546bdaa9ac2e69edf3fff
dCypherNx <32493242+dCypherNx@users.noreply.github.com>
2026-08-30 23:25:48 -0300

    Extract dashboard visual tokens

---
+++ app/src/main/java/br/com/t4acontrol/ui/dashboard/DashboardTokens.kt
@@ -0,0 +1,49 @@
+package br.com.t4acontrol.ui.dashboard
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object T4ADashboardTokens {
+    val CardSpacing: Dp = 10.dp
+    val CardPadding: Dp = 10.dp
+    val CardRadius: Dp = 16.dp
+    val CardElevation: Dp = 2.dp
+    val ModeRadius: Dp = 12.dp
+    val ToggleRadius: Dp = 14.dp
+    val ActionRadius: Dp = 12.dp
+    val ModeHeight: Dp = 76.dp
+    val ToggleHeight: Dp = 76.dp
+    val ActionHeight: Dp = 82.dp
+    val SpeedGaugeHeight: Dp = 154.dp
+    val BatteryHeight: Dp = 18.dp
+    val SecondRowIconSize: Dp = 31.dp
+    val SpeedFontSize = 104.sp
+    val SpeedUnitFontSize = 24.sp
+    val SectionTitleFontSize = 19.sp
+    val ModeFontSize = 12.sp
+    val ModeSpeedFontSize = 11.sp
+    val ControlFontSize = 10.sp
+    val ActionFontSize = 11.sp
+    val MetricFontSize = 11.sp
+    val Navy = Color(0xFF00133D)
+    val Blue = Color(0xFF075EF0)
+    val Green = Color(0xFF00A529)
+    val Red = Color(0xFFE91925)
+    val Cyan = Color(0xFF08A6B7)
+    val Orange = Color(0xFFFF8A00)
+    val Amber = Color(0xFFFFB300)
+    val DarkScaleBlue = Color(0xFF8AB4FF)
+    val LightBackground = Color(0xFFF7F8FB)
+    val DarkBackground = Color(0xFF0D1117)
+    val LightSurface = Color.White
+    val DarkSurface = Color(0xFF171D26)
+    val LightOutline = Color(0xFFE4E8F0)
+    val DarkOutline = Color(0xFF354052)
+    val LightForeground = Color(0xFF101820)
+    val DarkForeground = Color(0xFFF1F5F9)
+    val LightMuted = Color(0xFF667085)
+    val DarkMuted = Color(0xFFAAB4C3)
+    val EmptySegment = Color(0xFFE8EBF0)
+}
