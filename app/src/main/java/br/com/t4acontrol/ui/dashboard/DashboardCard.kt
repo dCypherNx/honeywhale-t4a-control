9db9ce046ecd3c76b942e8be683fc600f45c2620
dCypherNx <32493242+dCypherNx@users.noreply.github.com>
2026-08-30 23:27:29 -0300

    Fix dashboard card dimension import

--- app/src/main/java/br/com/t4acontrol/ui/dashboard/DashboardCard.kt
+++ app/src/main/java/br/com/t4acontrol/ui/dashboard/DashboardCard.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun DashboardCard(surface: Color, outline: Color, content: @Composable ColumnScope.() -> Unit) {
