0c4e3b1ded578b4b948ebaea20d33e56744581bd
dCypherNx <32493242+dCypherNx@users.noreply.github.com>
2026-08-30 23:34:56 -0300

    Add login Compose preview

--- app/src/main/java/br/com/t4acontrol/ui/login/LoginScreen.kt
+++ app/src/main/java/br/com/t4acontrol/ui/login/LoginScreen.kt
@@ -1,6 +1,8 @@
 package br.com.t4acontrol.ui.login
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,6 +16,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.t4acontrol.R
 
@@ -45,5 +49,13 @@ internal fun LoginScreen(foreground: Color, onLogin: (String, String) -> Unit) {
         modifier = Modifier.fillMaxWidth(),
     ) {
         Text(stringResource(R.string.login))
+    }
+}
+
+@Preview(showBackground = true, widthDp = 412)
+@Composable
+private fun LoginScreenPreview() {
+    Column(Modifier.padding(16.dp)) {
+        LoginScreen(Color(0xFF101820)) { _, _ -> }
     }
 }
