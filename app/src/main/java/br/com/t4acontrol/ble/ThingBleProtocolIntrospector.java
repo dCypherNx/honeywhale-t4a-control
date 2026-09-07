1449a9cd807eb1d673ecbc4d33c755629402a8c4
dCypherNx <32493242+dCypherNx@users.noreply.github.com>
2026-09-06 21:53:55 -0300

    Trace BLE builder protocol selection metadata

--- app/src/main/java/br/com/t4acontrol/ble/ThingBleProtocolIntrospector.java
+++ app/src/main/java/br/com/t4acontrol/ble/ThingBleProtocolIntrospector.java
@@ -16,8 +16,11 @@ public final class ThingBleProtocolIntrospector {
   private static final AtomicBoolean RAN = new AtomicBoolean(false);
   private static final String[] TARGETS = {
       "com.thingclips.smart.android.ble.builder.BleConnectBuilder",
+      "com.thingclips.smart.android.ble.builder.BleConnectBuilder$ExtInfo",
+      "com.thingclips.smart.android.ble.builder.BleConnectBuilder$Level",
       "com.thingclips.smart.android.ble.IThingBleManager",
       "com.thingclips.smart.interior.device.bean.DeviceRespBean",
+      "com.thingclips.smart.interior.device.bean.DeviceRespBean$ProductModule",
       "com.thingclips.sdk.ble.core.ability.options.BleConnectParams",
       "com.thingclips.sdk.ble.core.protocol.entity.ConnectParam",
       "com.thingclips.sdk.ble.core.protocol.entity.ConnectOpt",
